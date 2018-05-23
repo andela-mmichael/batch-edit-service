2bbd876934704af94675da53d0a7fb462ef60854
Mel Michael <mel.michael@andela.com>
2018-05-23 14:00:52 +0100

    Implement update functionality

--- index.js
+++ index.js
@@ -26,12 +26,76 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/', function (req, res) {
-  res.send(200, 'App is running well');
+  res.send(200, "You're running the Batch Edit Service");
 });
 
-app.post('/batch', function (req, res) {
-  res.json({ message: 'We got here' });
+
+/**
+ *
+ * Example:
+ *
+ *    POST /batch/:id?
+ *
+ *  where id refer
+ *
+ * Request:  {
+ *  "url": "https://virtserver.swaggerhub.com/Guesty/Users/1.0.0/user",
+ *  "verb": "PUT",
+ *  "payload": [{
+    *  "id": "ja2S-hs81-ksn3-iQI9",
+    *  "body": { "age": 30 },
+    * }
+    *  {
+    *  "id": "ja2S-hs81-ksn3-iQI9",
+    *  "body": {"age": 30 }
+    *  },
+    *  {
+    *    "id": "ja2S-hs81-ksn3-iQI9",
+    *    "body": {"age": 30 }
+    *  }
+ *  ]
+ * }
+
+
+
+ *
+ * Response:
+ *
+ *   Returns a result request
+ *
+ *
+ * @param req - HTTP request object
+ * @param res - HTTP response object
+ */
+
+app.post('/batch/:id?', function (req, res) {
+  let result = [];
+  let data = req.body.payload || req.params.id;
+
+  data.forEach(async(element) => {
+    const requestOptions = {
+      url: `${req.body.url}/${element.id}`,
+      type: req.body.verb
+    };
+    let message = await updateUser(requestOptions, element);
+    result.push({ id: element.id, status: message });
+  });
+
+  res.json({ result });
+
 });
+
+
+function updateUser(options, payload) {
+  return new Promise((resolve, reject) =>  {
+    unirest.put(options.url)
+      .headers({ 'Accept': 'application/json', 'Content-Type': 'application/json' })
+      .send(payload.body)
+      .end(function (response) {
+        resolve(response.body);
+      })
+  });
+}
 
 
 /**
